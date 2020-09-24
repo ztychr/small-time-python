ef2f3fb87b4fd2e707ebc34129febc6eacb0d088
ztychr <33706390+ztychr@users.noreply.github.com>
2020-09-24 18:16:32 +0200

    Add files via upload

---
+++ visitors.py
@@ -0,0 +1,16 @@
+visitors = open('visitors.txt', 'r')
+c = 1
+
+for line in visitors:
+    fields = line.strip().split()
+
+    status_code = fields[8]
+    requested = fields[6]
+
+    if status_code == "200":
+        if not ".svg" or ".img" or ".png" or ".css" in requested:
+	    if requested != "*":
+		c+=1
+		#print requested
+
+print c
