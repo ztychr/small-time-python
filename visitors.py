e981c1c47f613baea2db07d7752d6ddd25dfd1dd
ztychr <33706390+ztychr@users.noreply.github.com>
2020-09-26 16:14:21 +0200

    Update visitors.py

--- visitors.py
+++ visitors.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 
-if sys.argv[1] == "-h" or "--help":
-    print "usage: gunzip -c <access log.gz> | visitors.py"
-    sys.exit()
+parser = argparse.ArgumentParser(description='get visitor details from access logs by piping output into visitors.', usage='gunzip -c access.log.1.gz | visitors -i -c')
+parser.add_argument("-c", "--count", help="print amount of unique visitors", action="store_true")
+parser.add_argument("-i", "--ip", help="print unique ips", action="store_true")
+parser.add_argument("-s", "--sites", help="print visited sites", action="store_true")
+
+args = parser.parse_args()
 
 input = sys.stdin
-
-c = 0
+ips = []
+req = []
 
 for line in input:
     fields = line.strip().split()
 
-    status_code = fields[8]
+    ip = fields[0]
     requested = fields[6]
+    status_code = fields[8]
 
     if status_code == "200":
         if not ".svg" or ".img" or ".png" or ".css" in requested:
             if requested != "*":
-                c+=1
-                #print requested
+                if ip not in ips:
+                    ips.append(ip)
+                    req.append(requested)
 
-print c
+if args.sites:
+    print '\n'.join(req)
+
+if args.ip:
+    print '\n'.join(ips)
+
+if args.count:
+    print len(ips)
